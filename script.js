da2909275556dec4badfc0a6ced0765d836b33f8
jonasrodrigs <mexylemphotos@gmail.com>
2024-07-06 21:30:20 -0300

    primeiro projeto usando java script

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let form = document.querySelector(".formlario-fale-conosco")
+let mascara = document.querySelector(".mascara-formulario")
+
+function cliquei(){
+
+    
+    
+    form.style.left = ("50%")
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility="visible"
+}
+    
+
+
+let mascara1 = document.querySelector(".formlario-fale-conosco")
+let mascara2 = document.querySelector(".mascara-formulario")
+
+function java1(){
+
+    form.style.left = ("-317px")
+    form.style.transform = "translateX(0%)"
+    mascara.style.visibility="hidden"
+
+
+ 
+}
+
+
+
+
+
+
+
+
